05bb19ff32dbef4f528852aadd1ff492b342b5e8
SnakerWorker <snakerworker@gmail.com>
2014-09-19 22:12:08 +0800

    modify index.js

--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
   app.use('/api/categories', require('./routes/categories'));
   app.use('/api/items', require('./routes/items'));
+  app.use('/api/cart',require('./routes/cart'));
 };
