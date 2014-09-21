e9b106cefe98a7d653bf8ade3a599b86daf8c5e3
SnakerWorker <snakerworker@gmail.com>
2014-09-21 11:42:55 +0800

    modify post initems.js

--- router/routes/items.js
+++ router/routes/items.js
@@ -10,13 +10,13 @@ router.get('/', function(req, res) {
 
 router.post('/',function(req,res){
   var products = [
-    {id:1, name: 'Instant_noodles', unit: 'bag', category: '1', price: 1},
-    {id:2, name: 'apple', unit: 'kg', category: '1', price: 2.5},
-    {id:3, name: 'coca_cola', unit: 'bottle', category: '1', price: 0.5},
-    {id:4, name: 'kettle', unit: 'piece', category: '2', price: 43.5},
-    {id:5, name: 'fan', unit: 'piece', category: '2', price: 30}
+    {id:1, name: 'Instant_noodles', unit: 'bag', categoryId: '1', price: 1},
+    {id:2, name: 'apple', unit: 'kg', categoryId: '1', price: 2.5},
+    {id:3, name: 'coca_cola', unit: 'bottle', categoryId: '1', price: 0.5},
+    {id:4, name: 'kettle', unit: 'piece', categoryId: '2', price: 43.5},
+    {id:5, name: 'fan', unit: 'piece', categoryId: '2', price: 30}
   ];
-  var newProducts =  req.param('products') || products;
+  var newProducts =  req.body.products || products;
   storage.set('products',JSON.stringify(newProducts),function(err,obj){
     res.send(obj);
   });
