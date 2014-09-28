d99e479107022494a5dff4f3196790314d392d8c
SnakerWorker <snakerworker@gmail.com>
2014-09-28 14:39:26 +0800

    rename cart to cartItems

--- router/index.js
+++ router/index.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
   app.use('/api/categories', require('./routes/categories'));
   app.use('/api/items', require('./routes/items'));
-  app.use('/api/cart',require('./routes/cart'));
+  app.use('/api/cartItems',require('./routes/cartItems'));
 };
