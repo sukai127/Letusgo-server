b4939ed93a150c80b9583d4e824a2176b3afcba8
SnakerWorker <snakerworker@gmail.com>
2014-09-28 15:28:13 +0800

    add use payment

--- router/index.js
+++ router/index.js
@@ -2,4 +2,5 @@ module.exports = function(app) {
   app.use('/api/categories', require('./routes/categories'));
   app.use('/api/products', require('./routes/products'));
   app.use('/api/cartItems',require('./routes/cartItems'));
+  app.use('/api/payment',require('./routes/payment'));
 };
