e556cb3b50e6bcc58cb3c002295ffb8f5e9f218d
SnakerWorker <snakerworker@gmail.com>
2014-09-28 15:16:00 +0800

    add initProducts

--- router/routes/products.js
+++ router/routes/products.js
@@ -5,6 +5,22 @@ var storage = redis.createClient();
 
 var _ =  require('lodash');
 
+var initProducts = function(){
+  return [
+    {id:1, name: 'Instant_noodles', unit: 'bag', categoryId: '1', price: 1},
+    {id:2, name: 'apple', unit: 'kg', categoryId: '1', price: 2.5},
+    {id:3, name: 'coca_cola', unit: 'bottle', categoryId: '1', price: 0.5},
+    {id:4, name: 'kettle', unit: 'piece', categoryId: '2', price: 43.5},
+    {id:5, name: 'fan', unit: 'piece', categoryId: '2', price: 30}
+  ];
+};
+
+storage.get('products',function(err,data){
+  if(!data){
+    storage.set('products',JSON.stringify(initProducts()));
+  }
+});
+
 router.get('/', function(req, res) {
   storage.get('products',function(err,data){
     res.send(data);
@@ -87,7 +103,7 @@ router.put('/:id', function(req, res) {
         products[index] = product;
       }
     });
-    
+
     storage.set('products',JSON.stringify(products),function(err,obj){
       res.send(obj);
     });
