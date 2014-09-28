c7c9524212ad927ff8df7b44393b6684341c5c81
SnakerWorker <snakerworker@gmail.com>
2014-09-28 14:57:52 +0800

    format the code

--- router/routes/cartItems.js
+++ router/routes/cartItems.js
@@ -11,10 +11,14 @@ router.get('/', function(req, res) {
 });
 
 router.post('/',function(req,res){
+
   var cartItem =  req.body.cartItem;
+
   storage.get('cartItems',function(err,data){
+
     var cartItems = JSON.parse(data);
     cartItems.push(cartItem);
+    
     storage.set('cartItems',JSON.stringify(cartItems),function(err,obj){
       res.send(obj);
     });
@@ -29,12 +33,16 @@ router.delete('/',function(req,res){
 });
 
 router.delete('/:id', function(req, res) {
+
   var id = req.params.id;
+
   storage.get('cartItems',function(err,data){
+
     var cartItems = JSON.parse(data);
     _.remove(cartItems,function(cartitem){
       return cartitem.id.toString() === id.toString();
     });
+
     storage.set('cartItems',JSON.stringify(cartItems),function(err,obj){
       res.send(obj);
     });
@@ -42,15 +50,20 @@ router.delete('/:id', function(req, res) {
 });
 
 router.put('/:id', function(req, res) {
+
   var cartItem = req.body.cartItem;
   var id = req.params.id;
+
   storage.get('cartItems',function(err,data){
+
     var cartItems = JSON.parse(data);
+
     _.find(cartItems,function(item,index){
       if(item.id.toString() === id.toString()){
         cartItems[index] = cartItem;
       }
     });
+
     storage.set('cartItems',JSON.stringify(cartItems),function(err,obj){
       res.send(obj);
     });
