04797001754e4a4df64cce932b4a22a732cce37e
SnakerWorker <snakerworker@gmail.com>
2014-09-28 15:17:26 +0800

    modify post method

--- router/routes/products.js
+++ router/routes/products.js
@@ -61,27 +61,14 @@ router.delete('/:id', function(req, res) {
 
 router.post('/',function(req,res){
 
-  var initProducts = function(){
-    return [
-      {id:1, name: 'Instant_noodles', unit: 'bag', categoryId: '1', price: 1},
-      {id:2, name: 'apple', unit: 'kg', categoryId: '1', price: 2.5},
-      {id:3, name: 'coca_cola', unit: 'bottle', categoryId: '1', price: 0.5},
-      {id:4, name: 'kettle', unit: 'piece', categoryId: '2', price: 43.5},
-      {id:5, name: 'fan', unit: 'piece', categoryId: '2', price: 30}
-    ];
-  };
-
   var product = req.body.product;
-  var newProducts = initProducts();
 
   storage.get('products',function(err,data){
 
-    if(product){
-      newProducts = JSON.parse(data);
-      var id = newProducts[newProducts.length-1].id + 1;
-      product.id = id;
-      newProducts.push(product);
-    }
+    var newProducts = JSON.parse(data);
+    var id = newProducts[newProducts.length-1].id + 1;
+    product.id = id;
+    newProducts.push(product);
 
     storage.set('products',JSON.stringify(newProducts),function(err,obj){
       res.send(obj);
