75934bcb0b1217c6e69845fa095620c84347e242
SnakerWorker <snakerworker@gmail.com>
2014-09-28 15:27:11 +0800

    add post method

--- router/routes/payment.js
+++ router/routes/payment.js
@@ -1,1 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var redis = require("redis");
+var storage = redis.createClient();
 
+router.post('/', function (req, res) {
+  storage.del('cartItems', function () {
+    res.sendStatus(200);
+  });
+});
+
+module.exports = router;
